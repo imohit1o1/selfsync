b2a2279377e142567396dd79673fe34dff2f2c99
imohit1o1 <mohitk1595@gmail.com>
2024-12-06 21:13:29 +0530

    sfsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ThemeModeToggle } from "@/components/theme-toggle";
 export default function Home() {
   return (
     <div className="flex justify-between items-center max-w-7xl mx-auto p-4">
-      <nav>SelfSync</nav>
+      <nav>SelfSysdsnc</nav>
       <div className="flex gap-4">
         <ThemeModeToggle />
         <Button className="">Login</Button>
